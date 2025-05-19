e061f075184a94ec84c120cfb6a699bb59f4051d
Niki Joel <niki_ponce@hotmail.com>
2025-05-19 20:35:38 +0200

    Update environment variable for DB_URL_MERCADATA and adjust CORS settings in main.py

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],
+    allow_origins=[FRONTEND_URL, "http://mercadata.local"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
